c3a205e7ea011d05a802502155be738f083643f2
Alesia Branavitskaya <alesun4ik@gmail.com>
2024-10-07 19:53:24 +0300

    feat: create cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,29 @@
 const parseArgs = () => {
-    // Write your code here 
+    const indx = process.argv.findIndex((item) => {
+        return item.includes('--');
+    });
+
+    const argArr = process.argv.slice(indx);
+
+    const argvStringArr = [];    
+    const propArr = [];
+    const valueArr = [];
+    argArr.forEach((arg) => {
+        if(arg.includes('--')) {
+            propArr.push(arg);
+        } else {
+            valueArr.push(arg);
+        }
+    })
+    propArr.forEach((prop, i) => {
+        valueArr.forEach((value, ind) => {
+            if(i === ind) {
+                argvStringArr.push(`${prop} is ${value}`);
+            }
+        })
+    })
+
+    console.log(argvStringArr.join(', '));
 };
 
 parseArgs();
